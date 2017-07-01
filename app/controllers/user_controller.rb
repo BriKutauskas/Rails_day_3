b4c8936c2c8de4cda9852370fad5d6b46ea67901
Brianna Kutauskas <briannakutauskas@Briannas-MacBook-Pro.local>
2017-07-01 05:48:53 -0700

    functional user login without required

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,7 @@
 class UserController < ApplicationController
   #forces user to register with first and last name
   def register
-    if !params[:first] || !params[:last]
+    if !params.has_key?(:first) || params[:first].strip.empty? || !params.has_key?(:last) || params[:last].strip.empty?
       render "register"
     else
       user = User.new(params[:first], params[:last])
